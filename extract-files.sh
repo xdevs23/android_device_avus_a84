16acb831bae8102e1491c90daa055452c15a698e
Simao Gomes Viana <xdevs23@outlook.com>
2016-10-28 20:03:38 +0200

    remove zip file stuff

--- extract-files.sh
+++ extract-files.sh
@@ -11,25 +11,12 @@ DEVICE=a84
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
 rm -rf $BASE/*
 
-if [ -z "$2" ]; then
-    ZIPFILE=../../../${DEVICE}_update.zip
-else
-    ZIPFILE=$2
+for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
+    DIR=`dirname $FILE`
+if [ ! -d $BASE/$DIR ]; then
+    mkdir -p $BASE/$DIR
 fi
+    adb pull /system/$FILE $BASE/$FILE
+done
 
-if [ "$1" = "unzip" -a ! -e $ZIPFILE ]; then
-    echo $ZIPFILE does not exist.
-else
-    for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
-        DIR=`dirname $FILE`
-	if [ ! -d $BASE/$DIR ]; then
-            mkdir -p $BASE/$DIR
-	fi
-	if [ "$1" = "unzip" ]; then
-            unzip -j -o $ZIPFILE system/$FILE -d $BASE/$DIR
-	else
-            adb pull /system/$FILE $BASE/$FILE
-	fi
-    done
-fi
 ./setup-makefiles.sh
